c7aaaf98ea48bf9b75d97a7dcbd91007980c371c
Rebecca Baumher <bbaumher@gmail.com>
2024-11-15 14:47:07 -0700

    default game should use txt file dictionary

--- spelling_bee_server.py
+++ spelling_bee_server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 from spellingBee import SpellingBee
 
-bee = SpellingBee(['c', 'h', 'a', 'n', 'g', 't', 'i'], 't', False)
+bee = SpellingBee(['c', 'h', 'a', 'n', 'g', 't', 'i'], 't', True)
 game = dict(
 	letters=bee.characters,
     main_letter=bee.main_letter,
